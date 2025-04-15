b12b033b6613909e024b7e265b4a5f6466516423
Sean Ezrol <garrulousbrevity@gmail.com>
2025-04-15 19:28:00 -0400

    for now, for us, for now, expose swagger

--- SecretSipsServer/Program.cs
+++ SecretSipsServer/Program.cs
@@ -17,11 +17,11 @@ builder.Services.AddSingleton<GameDAO>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 app.UseWebSockets();
 
 app.UseHttpsRedirection();
